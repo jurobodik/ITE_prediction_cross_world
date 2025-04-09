cee7bce0e5a9537601a67b9e2d054d4755c2aede
Juraj Bodik <81770028+jurobodik@users.noreply.github.com>
2025-04-09 20:31:56 +0200

    Update and rename D_rho_intervals.py to D_rho_intervals_function.py

--- D_rho_intervals_function.py
+++ D_rho_intervals_function.py
@@ -1,7 +1,4 @@
 ################################################################################
-# D_rho_intervals.py
-#
-# Title: ITE Prediction Intervals under Cross-World Assumptions
 # Author: jurajbodik.com
 # Description:
 #   This script provides an implementation of prediction intervals 
